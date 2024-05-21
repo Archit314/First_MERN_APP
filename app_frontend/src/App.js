2f62595053b652eb04efed1cdb247c19c9c96576
Archit Kumar <architkumar314@gmail.com>
2024-05-22 01:01:18 +0530

    Context implementation started for authentication

--- app_frontend/src/App.js
+++ app_frontend/src/App.js
@@ -1,7 +1,33 @@
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import LandingPage from "./Components/NavLinks/LandingPage";
+import { authContext } from "./Components/Context/AuthContext";
+import { useState } from "react";
 
 function App() {
-  return <h1>hello</h1>;
+  const [isLoggedIn, SetIsLoggedIn] = useState(false);
+
+  const login = () => {
+    SetIsLoggedIn(true);
+  };
+  const logout = () => {
+    SetIsLoggedIn(false);
+  };
+  return (
+    <>
+      <authContext.Provider
+        value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+      >
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+          </Routes>
+        </BrowserRouter>
+      </authContext.Provider>
+    </>
+  );
 }
 
 export default App;
