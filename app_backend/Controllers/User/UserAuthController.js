ce2a5c1da7533d219a1173584e1d05ccb5dba2ca
Archit Kumar <architkumar314@gmail.com>
2024-05-23 22:34:07 +0530

    Added few more logic for user sign-in and sign-up

--- app_backend/Controllers/User/UserAuthController.js
+++ app_backend/Controllers/User/UserAuthController.js
@@ -1,22 +1,32 @@
 // Third party importing
-const HttpError = require('../../models/http-error')
+const HttpError = require("../../models/http-error");
 
 // Below is the function to handle user sign-up
 const userSignUp = (req, res, next) => {
-    console.log(`User sign-up API calls...........`);
-    res.json({message: `User sign-up successfully`})
-}
+  const { email, password } = req.body;
+  if (!email || !password) {
+    const message = !email ? "Email is required" : "Password is required";
+
+    console.log(message);
+    return res.status(422).json({ message: message, status: 422 });
+  }
+
+  console.log(`User sign-up API calls...........`);
+  return res
+    .status(200)
+    .json({ message: `User sign-up successfully`, status: 200 });
+};
 
 // Practice route
 const homeRoute = (req, res, next) => {
-    // If some error occured in any specific route then it will be thrown out to app.js file and from there app.use middle ware will handle the error:
-    throw new HttpError(`Could not found`, 404)
+  // If some error occured in any specific route then it will be thrown out to app.js file and from there app.use middle ware will handle the error:
+  throw new HttpError(`Could not found`, 404);
 
-    // Use when we do not have error hanling that we have created in http-error.js file and app.use middleware in the app.js file:
-    // const error = new Error('Could not found')
-    // error.code = 404
-    // throw error
-}
+  // Use when we do not have error hanling that we have created in http-error.js file and app.use middleware in the app.js file:
+  // const error = new Error('Could not found')
+  // error.code = 404
+  // throw error
+};
 
-exports.userSignUp = userSignUp
-exports.homeRoute = homeRoute
+exports.userSignUp = userSignUp;
+exports.homeRoute = homeRoute;
--- app_backend/app.js
+++ app_backend/app.js
@@ -1,23 +1,32 @@
 // Third party imports
-const express = require('express')
-const bodyParser = require('body-parser')
-require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 // File imports
-const userAuthRoute = require('./routes/UserAuthRoute')
+const userAuthRoute = require("./routes/UserAuthRoute");
+const HttpError = require("./models/http-error");
 
-const app = express()
+const app = express();
+
+app.use(bodyParser.json());
+
 console.log(`server started`);
-app.use('/v1/api/auth/user', userAuthRoute)
+app.use("/v1/api/auth/user", userAuthRoute);
+
+app.use((req, res, next) => {
+  const error = new HttpError("Oops! Could not find this route.", 404);
+  throw error;
+});
 
 app.use((error, req, res, next) => {
-    if(res.headerSent){
-        console.log('got error');
-        return next(error)
-    }
-    console.log('errors');
-    res.status(error.code || 500)
-    res.json({message: error.message || `An unknown error occured!`})
-})
+  if (res.headerSent) {
+    console.log("got error");
+    return next(error);
+  }
+  console.log("errors");
+  res.status(error.code || 500);
+  res.json({ message: error.message || `An unknown error occured!` });
+});
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT);
