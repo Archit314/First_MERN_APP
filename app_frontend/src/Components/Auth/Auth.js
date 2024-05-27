769c832eacb49968bd5559c8f9bc45d9ad58ae11
architKumar314 <architkumar314@gmail.com>
2024-05-27 13:14:01 +0530

    Added toast message in sign-in and sign-up API

--- app_frontend/src/Components/Auth/Auth.js
+++ app_frontend/src/Components/Auth/Auth.js
@@ -2,6 +2,7 @@ import React, { useContext, useState } from "react";
 import { authContext } from "../Context/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import Toast from "../Toast/Toast";
 
 export default function Auth() {
   const auth = useContext(authContext);
@@ -25,6 +26,13 @@ export default function Auth() {
     SetData({ ...data, [name]: value });
   };
 
+  const [showToastMessage, SetShowToastMessage] = useState(false)
+  const [errorMessage, SetErrorMessage] = useState('')
+
+  const handleClose = () => {
+    SetShowToastMessage(false)
+  }
+
   const handleSubmit = async (event) => {
     event.preventDefault();
 
@@ -47,14 +55,22 @@ export default function Auth() {
       try {
         const response = await axios(config);
 
-        if (response && response.status === 200) {
+        if (response && response.data.status === 200) {
           console.log(`User sign-in successfully`);
-          navigate("/home");
+          SetShowToastMessage(true)
+          SetErrorMessage(response.data.message)
+          auth.login()
+          // navigate("/home");
         } else {
           console.log(`User sign-in failed`);
         }
       } catch (error) {
         console.log(error);
+
+        if (error.response.data.status !== 200) {
+          SetShowToastMessage(true)
+          SetErrorMessage(error.response.data.message)
+        }
       }
     } else {
       const postData = {
@@ -78,105 +94,115 @@ export default function Auth() {
       try {
         const response = await axios(config);
 
-        if (response.status === 200) {
+        if (response && response.data.status === 200) {
           console.log(`User sign-up successfully`);
+          SetShowToastMessage(true)
+          SetErrorMessage(response.data.message)
           auth.login();
           navigate("/home");
         } else {
           console.log(`User sign-up failed`);
         }
-      } catch (error) {}
+      } catch (error) {
+        if(error.response.data.status !== 200){
+          SetShowToastMessage(true)
+          SetErrorMessage(error.response.data.message)
+        }
+       }
     }
   };
 
   return (
-    <div className="container d-flex justify-content-center align-items-center vh-100">
-      <div className="w-50 p-4 border rounded">
-        <form onSubmit={handleSubmit}>
-          <div className="mb-3 row">
-            <label htmlFor="staticEmail" className="col-sm-2 col-form-label">
-              Email
-            </label>
-            <div className="col-sm-10">
-              <input
-                type="text"
-                className="form-control"
-                id="staticEmail"
-                name="email"
-                value={data.email}
-                onChange={handleOnChange}
-              />
-            </div>
-          </div>
-          <div className="mb-3 row">
-            <label htmlFor="inputPassword" className="col-sm-2 col-form-label">
-              Password
-            </label>
-            <div className="col-sm-10">
-              <input
-                type="password"
-                className="form-control"
-                id="inputPassword"
-                name="password"
-                value={data.password}
-                onChange={handleOnChange}
-              />
-            </div>
-          </div>
-          {!isSignup && (
+    <>
+      <div className="container d-flex justify-content-center align-items-center vh-100">
+        <div className="w-50 p-4 border rounded">
+          <form onSubmit={handleSubmit}>
             <div className="mb-3 row">
-              <label htmlFor="inputName" className="col-sm-2 col-form-label">
-                Name
+              <label htmlFor="staticEmail" className="col-sm-2 col-form-label">
+                Email
               </label>
               <div className="col-sm-10">
                 <input
                   type="text"
                   className="form-control"
-                  id="inputName"
-                  name="name"
-                  value={data.name}
+                  id="staticEmail"
+                  name="email"
+                  value={data.email}
                   onChange={handleOnChange}
                 />
               </div>
             </div>
-          )}
-          {!isSignup && (
             <div className="mb-3 row">
-              <label
-                htmlFor="inputPhoneNumber"
-                className="col-sm-2 col-form-label"
-              >
-                Phone Number
+              <label htmlFor="inputPassword" className="col-sm-2 col-form-label">
+                Password
               </label>
               <div className="col-sm-10">
                 <input
-                  type="tel"
+                  type="password"
                   className="form-control"
-                  id="inputPhoneNumber"
-                  name="mobileNumber"
-                  value={data.mobileNumber}
+                  id="inputPassword"
+                  name="password"
+                  value={data.password}
                   onChange={handleOnChange}
                 />
               </div>
             </div>
-          )}
-          <button type="submit" className="btn btn-light">
-            {isSignup ? "Sign-in" : "Sign-up"}
-          </button>
-          <p>
-            {isSignup ? "Don't have an account?" : "Already have an account?"}{" "}
-            <Link
-              type="button"
-              className="btn btn-light"
-              onClick={() => {
-                SetIsSignup(!isSignup);
-              }}
-            >
-              {isSignup ? "Sign-up" : "Sign-in"}
-            </Link>
-          </p>
-        </form>
+            {!isSignup && (
+              <div className="mb-3 row">
+                <label htmlFor="inputName" className="col-sm-2 col-form-label">
+                  Name
+                </label>
+                <div className="col-sm-10">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="inputName"
+                    name="name"
+                    value={data.name}
+                    onChange={handleOnChange}
+                  />
+                </div>
+              </div>
+            )}
+            {!isSignup && (
+              <div className="mb-3 row">
+                <label
+                  htmlFor="inputPhoneNumber"
+                  className="col-sm-2 col-form-label"
+                >
+                  Phone Number
+                </label>
+                <div className="col-sm-10">
+                  <input
+                    type="tel"
+                    className="form-control"
+                    id="inputPhoneNumber"
+                    name="mobileNumber"
+                    value={data.mobileNumber}
+                    onChange={handleOnChange}
+                  />
+                </div>
+              </div>
+            )}
+            <button type="submit" className="btn btn-light">
+              {isSignup ? "Sign-in" : "Sign-up"}
+            </button>
+            <p>
+              {isSignup ? "Don't have an account?" : "Already have an account?"}{" "}
+              <Link
+                type="button"
+                className="btn btn-light"
+                onClick={() => {
+                  SetIsSignup(!isSignup);
+                }}
+              >
+                {isSignup ? "Sign-up" : "Sign-in"}
+              </Link>
+            </p>
+          </form>
+        </div>
       </div>
-    </div>
+      {showToastMessage && <Toast errorMessage={errorMessage} onClose={handleClose}/>}
+    </>
   );
 }
--- app_frontend/src/Components/Toast/Toast.js
+++ app_frontend/src/Components/Toast/Toast.js
@@ -1,38 +1,17 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 
-export default function Toast({ message }) {
-  const toastRef = useRef(null);
+export default function Toast(props) {
 
-  useEffect(() => {
-    const toastElement = toastRef.current;
-    const bootstrapToast = window.bootstrap.Toast;
-    if (toastElement && bootstrapToast) {
-      const toast = new bootstrapToast(toastElement, {
-        autohide: true,
-        delay: 3000,
-      });
-      toast.show();
-    }
-  }, [message]);
   return (
-    <div>
-      <div
-        ref={toastRef}
-        className="toast align-items-center text-bg-primary border-0"
-        role="alert"
-        aria-live="assertive"
-        aria-atomic="true"
-      >
+    <>
+      <div className="toast align-items-center text-bg-primary border-0 show" role="alert" aria-live="assertive" aria-atomic="true">
         <div className="d-flex">
-          <div className="toast-body">{message}</div>
-          <button
-            type="button"
-            className="btn-close btn-close-white me-2 m-auto"
-            data-bs-dismiss="toast"
-            aria-label="Close"
-          ></button>
+          <div className="toast-body">
+            {props.errorMessage}
+          </div>
+          <button type="button" className="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close" onClick={props.onClose}></button>
         </div>
       </div>
-    </div>
+    </>
   );
 }
