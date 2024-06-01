6eed381b036f18cd3ac301d03db2bb1952727685
Archit Kumar <architkumar314@gmail.com>
2024-06-01 14:50:40 +0530

    Partila binding  profile data done

--- app_frontend/src/Components/User/UserProfile.js
+++ app_frontend/src/Components/User/UserProfile.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import "../../Components-CSS/User/UserProfileCss.css"
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 export default function UserProfile() {
+  const [data, SetData] = useState('')
   useEffect(() => {
     getUserProfile()
   }, [])
@@ -19,12 +20,15 @@ export default function UserProfile() {
 
     try {
       const response = await axios(config)
-      console.log(response);
+      // console.log(response);
       if (response.data.status === 200) {
+        console.log(`Profile fetched successfully`);
         console.log(response.data.data)
+        SetData(response.data.data)
       }
     }
     catch (error) {
+      console.log(`Error while fetching profile`);
       console.log(error);
     }
   }
@@ -35,49 +39,49 @@ export default function UserProfile() {
           <div className="row g-0 h-100">
             <div className="col-md-4 cardBody-left d-flex flex-column align-items-center justify-content-center">
               <h5>Your profile</h5>
-              <h5>Name</h5>
+              <h5> {data.name}</h5>
               <Link type="button" className="btn btn-light">Edit</Link>
             </div>
             <div className="col-md-8 cardBody-right">
               <div className="card-body" style={{ marginTop: '3rem' }}>
                 <div className='mb-5'>
                   <h5 className="card-title">INFORMATION</h5><hr></hr>
-                  <div class="row text-center">
-                    <div class="col">
-                      Email:
+                  <div className="row text-center">
+                    <div className="col">
+                      Email: {data.email}
                     </div>
-                    <div class="col">
-                      Phone:
+                    <div className="col">
+                      Phone: {data.mobileNumber}
                     </div>
                   </div>
 
                 </div>
                 <div className='mb-5'>
                   <h5 className="card-title">PROJECTS</h5><hr></hr>
-                  <div class="row text-center">
-                    <div class="col">
-                      Email:
+                  <div className="row text-center">
+                    <div className="col">
+                      Email: {data.email}
                     </div>
-                    <div class="col">
-                      Phone:
+                    <div className="col">
+                      Phone: {data.mobileNumber}
                     </div>
                   </div>
 
                 </div>
                 <div>
                   <h5 className="card-title">LINKS</h5><hr></hr>
-                  <div class="container text-center">
-                    <div class="row">
-                      <div class="col">
+                  <div className="container text-center">
+                    <div className="row">
+                      <div className="col">
                         Twitter
                       </div>
-                      <div class="col">
+                      <div className="col">
                         facebook
                       </div>
-                      <div class="col">
+                      <div className="col">
                         Github
                       </div>
-                      <div class="col">
+                      <div className="col">
                         Github
                       </div>
                     </div>
