97420ec67cadf94742813b34cf0176110fa82db4
Archit Kumar <architkumar314@gmail.com>
2024-06-01 13:07:53 +0530

    Fixes for the profile section UI

--- app_frontend/src/Components/User/UserProfile.js
+++ app_frontend/src/Components/User/UserProfile.js
@@ -8,43 +8,56 @@ export default function UserProfile() {
       <div className="d-flex justify-content-center align-items-center min-vh-100 cardBody">
         <div className="card mb-3" style={{ height: '70vh', width: '60%' }}>
           <div className="row g-0 h-100">
-            <div className="col-md-4 cardBody-left">
+            <div className="col-md-4 cardBody-left d-flex flex-column align-items-center justify-content-center">
               <h5>Your profile</h5>
-              <div className='d-flex justify-content-center align-items-center'>
-                <h5>Name</h5>
-              </div>
-              <div className='d-flex justify-content-center'>
-                <Link type="button" className="btn btn-light">Edit</Link>
-              </div>
+              <h5>Name</h5>
+              <Link type="button" className="btn btn-light">Edit</Link>
             </div>
             <div className="col-md-8 cardBody-right">
-              <div className="card-body">
-                <h5 className="card-title">Information</h5><hr></hr>
-                <div className='d-flex mb-5'>
-                  <div>email</div>
-                  <div>phone</div>
-                </div>
-                <h5 className="card-title">Projects</h5><hr></hr>
-                <div className='d-flex mb-5'>
-                  <div>email</div>
-                  <div>phone</div>
-                </div>
-                <h5 className="card-title">Links</h5><hr></hr>
-                <div class="container text-center">
-                  <div class="row">
+              <div className="card-body" style={{ marginTop: '3rem' }}>
+                <div className='mb-5'>
+                  <h5 className="card-title">INFORMATION</h5><hr></hr>
+                  <div class="row text-center">
                     <div class="col">
-                      Twitter
+                      Email:
                     </div>
                     <div class="col">
-                      facebook
-                    </div>
-                    <div class="col">
-                      Github
-                    </div>
-                    <div class="col">
-                      Github
+                      Phone:
                     </div>
                   </div>
+
+                </div>
+                <div className='mb-5'>
+                  <h5 className="card-title">PROJECTS</h5><hr></hr>
+                  <div class="row text-center">
+                    <div class="col">
+                      Email:
+                    </div>
+                    <div class="col">
+                      Phone:
+                    </div>
+                  </div>
+
+                </div>
+                <div>
+                  <h5 className="card-title">LINKS</h5><hr></hr>
+                  <div class="container text-center">
+                    <div class="row">
+                      <div class="col">
+                        Twitter
+                      </div>
+                      <div class="col">
+                        facebook
+                      </div>
+                      <div class="col">
+                        Github
+                      </div>
+                      <div class="col">
+                        Github
+                      </div>
+                    </div>
+                  </div>
+
                 </div>
               </div>
             </div>
